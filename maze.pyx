856ed8c50ac7f1a322115f3f3c4374535bfe78cb
Marek Suchánek <suchama4@fit.cvut.cz>
2016-12-03 17:19:44 +0100

    Using C++ queue for flood

--- maze.pyx
+++ maze.pyx
@@ -1,8 +1,16 @@
 import numpy as np
-from collections import deque
+from libcpp.queue cimport queue
 
+cdef struct coords:
+    int x
+    int y
 
-class NoPathExistsException(Exception):
+cdef struct qitem:
+    int x
+    int y
+    int distance
+
+cdef class NoPathExistsException(Exception):
     pass
 
 class MazeAnalysis:
@@ -13,26 +21,26 @@ class MazeAnalysis:
         self.directions = np.full(maze.shape, b' ', dtype=('a', 1))
         np.place(self.directions, maze < 0, b'#')
         goals = np.asarray(np.where(maze == 1)).T
-        stack_open = deque()
+        cdef queue[qitem] q
         shape = maze.shape
         for goal in goals:
             g = tuple(goal)
             self.distances[g] = 0
             self.directions[g] = b'X'
-            stack_open.append((g, 0))
+            q.push(qitem(g[0],g[1],0))
         dir_offsets = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         dir_chars = [b'^', b'v', b'<', b'>']
-        while stack_open:
-            (x, y), distance = stack_open.popleft()
-            distance += 1
+        while not q.empty():
+            item = q.front()
+            q.pop()
             for i in range(4):
                 offset_x, offset_y = dir_offsets[i]
-                neighbor = x + offset_x, y + offset_y
+                neighbor = item.x + offset_x, item.y + offset_y
                 if 0 <= neighbor[0] < shape[0] and 0 <= neighbor[1] < shape[1]:
                     if maze[neighbor] >= 0 and self.distances[neighbor] == -1:
-                            self.distances[neighbor] = distance
+                            self.distances[neighbor] = item.distance+1
                             self.directions[neighbor] = dir_chars[i]
-                            stack_open.append((neighbor, distance))
+                            q.push(qitem(neighbor[0], neighbor[1], item.distance+1))
         self.is_reachable = b' ' not in self.directions
 
     def path(self, row, column):
@@ -53,5 +61,5 @@ class MazeAnalysis:
         return path
 
 
-def analyze(maze):
+cpdef analyze(maze):
     return MazeAnalysis(maze)
