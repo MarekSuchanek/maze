1560983b9fb63d3aaa34a2b13337dcb8a92f786c
Marek Suchánek <suchama4@fit.cvut.cz>
2016-12-26 13:40:50 +0100

    Jumper & teleporter actors working

--- maze/actors.py
+++ maze/actors.py
@@ -160,10 +160,9 @@ class ActorWithSpeed(Actor):
 
     async def behavior(self):
         while True:
-            shape = self.grid.analysis.directions.shape
             row = int(self.row)
             column = int(self.column)
-            if 0 <= row < shape[0] and 0 <= column < shape[1]:
+            if self.grid.inside_array(row, column):
                 direction = self.grid.analysis.directions[row, column]
             else:
                 direction = b'?'
@@ -226,7 +225,86 @@ class AcceleratorActor(ActorWithSpeed):
 # goal at least by 5 steps and more than 5 steps from goal. Jumper must walk at
 # least 10 steps before jumping.
 class JumperActor(Actor):
-    pass  # TODO: implement
+    JUMP_PROB = 1.0
+    STEPS_TO_JUMP = 0
+    MIN_DIST_GOAL = 5
+    JUMP_DURATION = 1
+    D2 = [(-2, 0), (2, 0), (0, 2), (0, -2)]
+    D1 = [(-2, 0), (1, 0), (0, 1), (0, -1)]
+
+    def __init__(self, grid, row, column, kind):
+        super().__init__(grid, row, column, kind)
+        self.steps_without_jump = 0
+
+    async def behavior(self):
+        while True:
+            self.steps_without_jump += 1
+            row = int(self.row)
+            column = int(self.column)
+            if self.grid.inside_array(row, column):
+                direction = self.grid.analysis.directions[row, column]
+            else:
+                direction = b'?'
+
+            self.in_goal = direction == b'X'
+
+            jump_diff = self.pick_jump_target()
+            if self.can_jump_over(*jump_diff):
+                await self.jump_over(*jump_diff, self.JUMP_DURATION)
+            elif direction == b'v':
+                await self.step(1, 0)
+            elif direction == b'>':
+                await self.step(0, 1)
+            elif direction == b'^':
+                await self.step(-1, 0)
+            elif direction == b'<':
+                await self.step(0, -1)
+            else:
+                await self.jump()
+
+    def pick_jump_target(self):
+        row = int(self.row)
+        col = int(self.column)
+
+        for i in range(4):
+            if self.grid.inside_array(row+self.D2[i][0], col+self.D2[i][1]):
+                target_dist = self.grid.analysis.distances[row+self.D2[i][0], col+self.D2[i][1]]
+                between_dist = self.grid.analysis.distances[row+self.D1[i][0], col+self.D1[i][1]]
+                act_dist = self.grid.analysis.distances[row, col]
+                if act_dist > target_dist > self.MIN_DIST_GOAL and between_dist < 0:
+                    return self.D2[i]
+        return 0, 0
+
+    def can_jump_over(self, x, y):
+        return self.steps_without_jump > self.STEPS_TO_JUMP and \
+               ((x + y == -2) or (x + y == 2)) and \
+               random.random() < self.JUMP_PROB
+
+    async def jump_over(self, dr, dc, duration):
+        self.steps_without_jump = 0
+        start_row = self.row
+        start_col = self.column
+
+        if dc == 0:
+            for p in self._progress(duration):
+                with self._update_context():
+                    self.row = start_row + dr * p
+                    self.column = start_col - p * (1 - p)
+
+                await asyncio.sleep(duration / self.grid.cell_size)
+        else:
+            for p in self._progress(duration):
+                with self._update_context():
+                    self.row = start_row - p * (1 - p)
+                    self.column = start_col + dc * p
+
+                await asyncio.sleep(duration / self.grid.cell_size)
+
+        # Final update to the exact ending position (this should use integer
+        # arithmetic, so it avoids rounding errors)
+        with self._update_context():
+            self.row = start_row + dr
+            self.column = start_col + dc
 
 
 # Teleportér*
@@ -240,7 +318,74 @@ class JumperActor(Actor):
 # During the teleportation process the actor shivers a lot. Destination must be more than
 # 5 steps from goal.
 class TeleporterActor(Actor):
-    pass  # TODO: implement
+    TELEPORT_PROB = 0.2
+    MIN_DIST_GOAL = 5
+    MIN_DIST_ACTOR = 5
+    SHIVER_DURATION = 0.5
+
+    async def behavior(self):
+        while True:
+            row = int(self.row)
+            column = int(self.column)
+            if self.grid.inside_array(row, column):
+                direction = self.grid.analysis.directions[row, column]
+            else:
+                direction = b'?'
+
+            self.in_goal = direction == b'X'
+
+            if self.can_teleport():
+                await self.teleport(*self.pick_teleport_target())
+            elif direction == b'v':
+                await self.step(1, 0)
+            elif direction == b'>':
+                await self.step(0, 1)
+            elif direction == b'^':
+                await self.step(-1, 0)
+            elif direction == b'<':
+                await self.step(0, -1)
+            else:
+                await self.jump()
+
+    def can_teleport(self):
+        return not self.in_goal and random.random() < self.TELEPORT_PROB
+
+    async def teleport(self, row, col):
+        await self.shiver(self.SHIVER_DURATION)
+
+        with self._update_context():
+            self.row = row
+            self.column = col
+
+        await self.shiver(self.SHIVER_DURATION)
+
+    async def shiver(self, duration):
+        start_row = self.row
+        start_column = self.column
+
+        for p in self._progress(duration):
+            with self._update_context():
+                self.column = start_column - p * (random.random()-0.5) * 0.25
+                self.row = start_row - p * (random.random()-0.5) * 0.25
+
+            await asyncio.sleep(2*duration / self.grid.cell_size)
+
+        with self._update_context():
+            self.row = start_row
+            self.column = start_column
+
+    def pick_teleport_target(self):
+        shape = self.grid.analysis.directions.shape
+        row = int(self.row)
+        col = int(self.column)
+        while not self.good_teleport_target(row, col):
+            row = random.randrange(0, shape[0])
+            col = random.randrange(0, shape[1])
+        return row, col
+
+    def good_teleport_target(self, row, col):
+        return self.grid.analysis.distances[row, col] > self.MIN_DIST_GOAL and \
+               ((int(self.row) - row)**2 + (int(self.column) - col)**2) > self.MIN_DIST_GOAL**2
 
 
 # Zmatkář
@@ -257,6 +402,8 @@ class ScatterbrainActor(Actor):
     ANTI_DIRS_CHARS = [b'^', b'<', b'v', b'>']
 
     def mess_up(self, row, col, direction):
+        if direction not in self.DIRS_CHARS:
+            return direction
         possible_dirs = []
         shape = self.grid.analysis.directions.shape
         for i in range(4):
@@ -275,10 +422,9 @@ class ScatterbrainActor(Actor):
 
     async def behavior(self):
         while True:
-            shape = self.grid.analysis.directions.shape
             row = int(self.row)
             column = int(self.column)
-            if 0 <= row < shape[0] and 0 <= column < shape[1]:
+            if self.grid.inside_array(row, column):
                 direction = self.grid.analysis.directions[row, column]
             else:
                 direction = b'?'
